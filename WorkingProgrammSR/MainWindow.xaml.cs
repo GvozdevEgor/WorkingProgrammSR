8e9ab310df421a851e478fb3f07e34a0da8ed77b
Егор Гвоздев <gvozdev.e@outlook.com>
2018-12-02 21:20:16 +0300

    4

--- WorkingProgrammSR/MainWindow.xaml.cs
+++ WorkingProgrammSR/MainWindow.xaml.cs
@@ -26,28 +26,80 @@ namespace WorkingProgrammSR
         public MainWindow()
         {
             InitializeComponent();
+            SetColorDryContactSensor(0xAAAA);
         }
 
-        private void RadioButtonDiagnosticObject_Checked(object sender, RoutedEventArgs e)
+        private void RadioButtonDiscoveryMechanism_Checked(object sender, RoutedEventArgs e)
         {
-            if (RadioButtonDiscoveryMechanism.IsChecked == true)
+            
+        }
+
+        private void RadioButtonDiscoveryUnit_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RadioButtonPowerFrame_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        public void SetColorDryContactSensor(UInt16 data)
+        {
+            for (int i = 0; i < 16; i++)
             {
-                //RadioButtonNotStop.IsChecked = true;
-                //RadioButtonStopDR.IsEnabled = false;
-                //RadioButtonStopAngle.IsEnabled = false;
-                //RadioButtonStopStep.IsEnabled = false;
-            }
-            if (RadioButtonDiscoveryUnit.IsChecked == true)
-            {
-                //RadioButtonStopDR.IsEnabled = true;
-                //RadioButtonStopAngle.IsEnabled = true;
-                //RadioButtonStopStep.IsEnabled = true;
-            }
-            if (RadioButtonPowerFrame.IsChecked == true)
-            {
-                //RadioButtonStopDR.IsEnabled = true;
-                //RadioButtonStopAngle.IsEnabled = true;
-                //RadioButtonStopStep.IsEnabled = true;
+                bool stat = (data & (1 << i)) > 0;
+                switch(i)
+                {
+                    case 0:
+                        GridDR11.Background = stat ? On : Off;
+                        break;
+                    case 1:
+                        GridDR12.Background = stat ? On : Off;
+                        break;
+                    case 2:
+                        GridDR21.Background = stat ? On : Off;
+                        break;
+                    case 3:
+                        GridDR22.Background = stat ? On : Off;
+                        break;
+                    case 4:
+                        GridDR31.Background = stat ? On : Off;
+                        break;
+                    case 5:
+                        GridDR32.Background = stat ? On : Off;
+                        break;
+                    case 6:
+                        GridDR41.Background = stat ? On : Off;
+                        break;
+                    case 7:
+                        GridDR42.Background = stat ? On : Off;
+                        break;
+                    case 8:
+                        GridDZ11.Background = stat ? On : Off;
+                        break;
+                    case 9:
+                        GridDZ12.Background = stat ? On : Off;
+                        break;
+                    case 10:
+                        GridDZ21.Background = stat ? On : Off;
+                        break;
+                    case 11:
+                        GridDZ22.Background = stat ? On : Off;
+                        break;
+                    case 12:
+                        GridDD1.Background = stat ? On : Off;
+                        break;
+                    case 13:
+                        GridDD2.Background = stat ? On : Off;
+                        break;
+                    case 14:
+                        GridKS1.Background = stat ? On : Off;
+                        break;
+                    case 15:
+                        GridKS2.Background = stat ? On : Off;
+                        break;
+                }
             }
         }
     }
