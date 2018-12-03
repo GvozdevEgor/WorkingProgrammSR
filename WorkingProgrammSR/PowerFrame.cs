a016a78d974ae87582dcac536f718689193b12c0
Егор Гвоздев <gvozdev.e@outlook.com>
2018-12-03 16:42:10 +0300

    хз

---
+++ WorkingProgrammSR/PowerFrame.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkingProgrammSR
+{
+    class PowerFrame
+    {
+        public bool[] WorkStepMotor;
+        public bool[] Direction;
+        public static uint Speed;
+        public static bool EncoderChannel;
+        public UInt16[] Angle;
+        public UInt16 DryContactSensor;
+        public UInt16[] NullAngle;
+
+        public PowerFrame()
+        {
+            WorkStepMotor = new bool[4] { false, false, false, false };
+            Direction = new bool[4] { false, false, false, false };
+            Speed = 60;
+            EncoderChannel = false;
+
+
+        }
+    }
+}
